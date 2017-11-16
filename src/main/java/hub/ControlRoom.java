ca530c63971383fa2a6d59baf59b856152bdb7bf
andreadisst <andreadisst@iti.gr>
2017-11-16 18:20:25 +0200

    remove test

--- src/main/java/hub/ControlRoom.java
+++ src/main/java/hub/ControlRoom.java
@@ -28,82 +28,7 @@ public class ControlRoom {
     
     public static void main(String[] args)  {
         
-        String message = "{\n" +
-"  \"header\": {\n" +
-"    \"topicName\": \"TOP021_INCIDENT_REPORT\",\n" +
-"    \"topicMajorVersion\": 1,\n" +
-"    \"topicMinorVersion\": 0,\n" +
-"    \"sender\": \"SCAPP\",\n" +
-"    \"msgIdentifier\": 542854,\n" +
-"    \"sentUTC\": \"2018-01-01T12:00:00Z\",\n" +
-"    \"status\": \"Actual\",\n" +
-"    \"actionType\": \"Update\",\n" +
-"    \"specificSender\": \"citizen\",\n" +
-"    \"scope\": \"Restricted\",\n" +
-"    \"district\": \"Thessaloniki\",\n" +
-"    \"recipients\": \"\",\n" +
-"    \"code\": 0,\n" +
-"    \"note\": \"\",\n" +
-"    \"references\": \"\"\n" +
-"  },\n" +
-"  \"body\": {\n" +
-"    \"incidentOriginator\": \"SCAPP\",\n" +
-"    \"incidentID\": 542853,\n" +
-"    \"language\": \"en-US\",\n" +
-"    \"incidentCategory\": \"Other\",\n" +
-"    \"incidentType\": \"Other\",\n" +
-"    \"priority\": \"undefined\",\n" +
-"    \"severity\": \"Unknown\",\n" +
-"    \"certainty\": \"Observed\",\n" +
-"    \"startTimeUTC\": \"2018-01-01T12:00:00Z\",\n" +
-"    \"expirationTimeUTC\": \"2018-01-01T12:00:00Z\",\n" +
-"    \"title\": \"\",\n" +
-"    \"description\": \"The plaza is flooded. (If the citizen only provides some media files without any description, then this field will be omitted.)\",\n" +
-"    \"position\": {\n" +
-"      \"latitude\": 45.43417,\n" +
-"      \"longitude\": 12.33847\n" +
-"    },\n" +
-"    \"attachments\": [{\n" +
-"      \"attachmentName\": \"Floods.2016.CsQfCsGWIAAhe0c.jpg\",\n" +
-"      \"attachmentType\": \"image\",\n" +
-"      \"attachmentTimeStampUTC\": \"2018-01-01T12:00:00Z\",\n" +
-"      \"attachmentURL\": \"http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=Floods.2016.CsQfCsGWIAAhe0c.jpg\"\n" +
-"    }]\n" +
-"  }\n" +
-"}";
-                    System.out.println("Message consumed: " + message);
-                    
-                    Type type = new TypeToken<IncidentReport>() {}.getType();
-                    
-                    try{
-                        IncidentReport incidentReport = gson.fromJson(message, type);
-
-                        if( incidentReport.getHeader().getSender().equals("FRAPP") || incidentReport.getHeader().getSender().equals("SCAPP") ){
-                            
-                            for(Attachment attachment : incidentReport.getBody().getAttachments()){
-                                
-                                switch(attachment.getAttachmentType()){
-                                    case "image":
-                                        ImageRequester imageRequester = new ImageRequester(incidentReport, attachment);
-                                        imageRequester.start();
-                                        break;
-                                    case "video":
-                                        VideoRequester videoRequester = new VideoRequester(incidentReport, attachment);
-                                        videoRequester.start();
-                                        break;
-                                    case "audio":
-                                        //AudioRequester audioRequester = new AudioRequester(incidentReport, attachment);
-                                        //audioRequester.start();
-                                        break;
-                                }
-                            }
-                            
-                        }
-                    }catch(JsonSyntaxException e){
-                        System.out.println(e);
-                    }
-        
-        /*BusReader busReader = new BusReader();
+        BusReader busReader = new BusReader();
         KafkaConsumer<String, String> kafkaConsumer = busReader.getKafkaConsumer();
         kafkaConsumer.subscribe(Arrays.asList(Configuration.incident_report_topic));
         
@@ -148,7 +73,7 @@ public class ControlRoom {
             }
         } finally {
           kafkaConsumer.close(); 
-        }*/
+        }
     }
     
 }
